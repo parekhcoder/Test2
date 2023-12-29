4355293a222aba9a7f359f32654ed7e747990b13
parekhcoder <97449674+parekhcoder@users.noreply.github.com>
2023-12-29 20:03:57 +0530

    Update backup.sh

--- resources/backup.sh
+++ resources/backup.sh
@@ -54,6 +54,7 @@ if [ "$has_failed" = false ]; then
     for CURRENT_DATABASE in ${TARGET_DATABASE_NAMES//,/ }; do
 
         DUMP=$CURRENT_DATABASE$(date +$BACKUP_TIMESTAMP).sql
+        echo $DUMP
         # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
         if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $BACKUP_ADDITIONAL_PARAMS $BACKUP_CREATE_DATABASE_STATEMENT $CURRENT_DATABASE 2>&1 >/tmp/$DUMP); then
 
