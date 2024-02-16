7616bad75cc9a7181a31a5a810dc6023d220941d
parekhcoder <97449674+parekhcoder@users.noreply.github.com>
2024-02-16 17:14:56 +0530

    Create logging.sh

---
+++ resources/logging.sh
@@ -0,0 +1,61 @@
+# !/bin/bash
+
+function InitializeLog
+{
+    logJSON='{"Events":[]}'
+}
+
+function PushLog
+{  
+    curlOutput=$(curl -s -w "\n%{response_code}\n" -X POST $LOGAPIURL -H "Content-Type: application/json" -d "$logJSON")    
+    httpCode=$(tail -n1 <<< "$curlOutput")
+    curlOutput=$(sed '$ d' <<< "$curlOutput")
+
+    if [ "$httpCode" != 200 ];
+       then
+           echo "Error: while calling logging api- $curlOutput"      
+    fi      
+    
+} 
+
+function AddLog
+{
+    if [ -z "$logJSON" ];
+        then
+            logJSON='{"Events":[]}'
+    fi
+    cdate=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")
+    logMsg=$1
+    logLevel=$2
+    
+    case $logLevel in
+
+    D)
+        logLevel="Debug"
+    ;;
+
+    E)
+        logLevel="Error"
+    ;;
+
+    I)
+        logLevel="Information"
+    ;;
+
+    *)
+        logLevel="Debug"
+    ;;
+    esac  
+    
+    log=$(sed -e "s/##T##/$cdate/g;s/##level##/$logLevel/g" <<< $LOGFMT)   
+    logMsg=$(echo $logMsg|tr -d '\n')
+    logMsgCopy=$logMsg
+    if ! logMsg=$(jq '@json' <<< $logMsg 2>&1); then          
+   
+          logMsg=$(jq -R <<< $logMsgCopy)
+    fi
+   
+    log=$(jq --arg logMsg "$logMsg" ".\"@mt\" = $logMsg" <<< $log)        
+    echo "$log"
+    logJSON=$(jq ".Events[.Events | length] |= . + $log" <<< $logJSON)        
+}
