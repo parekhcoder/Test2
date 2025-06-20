ab6ea08d02a31a1ff4b20c8e6bf63c7cde1fc5bd
parekhcoder <97449674+parekhcoder@users.noreply.github.com>
2025-06-20 17:23:46 +0530

    Update setup_cron.sh

--- resources/setup_cron.sh
+++ resources/setup_cron.sh
@@ -8,21 +8,23 @@ echo "Using CRON_SCHEDULE: ${CRON_SCHEDULE}"
 crontab -r 2>/dev/null || true
 
 # Add the cron job
-(crontab -l 2>/dev/null; echo "${CRON_SCHEDULE} /app/backup.sh > /dev/null 2>&1") | crontab -
+#(crontab -l 2>/dev/null; echo "${CRON_SCHEDULE} /app/backup.sh > /dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "${CRON_SCHEDULE} /app/backup.sh >> /var/log/cron.log 2>&1") | crontab -
+
 
 echo "Crontab setup complete. Starting cron daemon."
 
-echo "Attempting to start rsyslogd..."
-rsyslogd -n & 
-RSYSLOG_PID=$!
-echo "rsyslogd started with PID: $RSYSLOG_PID"
+#echo "Attempting to start rsyslogd..."
+#rsyslogd -n & 
+#RSYSLOG_PID=$!
+#echo "rsyslogd started with PID: $RSYSLOG_PID"
 
-sleep 2
+#sleep 2
 
 # Start cron in the foreground
-echo "Starting cron daemon in foreground..."
-exec cron -f
+#echo "Starting cron daemon in foreground..."
+exec cron -f -d SCH,PROC,EXT
 
-echo "Error: Failed to start cron."
-kill $RSYSLOG_PID 
-exit 1
+#echo "Error: Failed to start cron."
+#kill $RSYSLOG_PID 
+#exit 1
